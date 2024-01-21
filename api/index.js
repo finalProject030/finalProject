eaa55f48c6eb0cc8707f334f1dbc0a55d208d1fa
guycohh <guycohh@google.com>
2024-01-21 18:17:20 +0200

    complete delete user listing functionality

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import path from "path";
 dotenv.config();
 
 mongoose
@@ -17,10 +16,10 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
-
 const app = express();
+
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
@@ -30,12 +29,6 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,8 +10,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex-wrap">
-            <span className=" text-slate-500">Guy</span>
-            <span className=" text-slate-700">Estate</span>
+            <span className=" text-slate-500">Stack</span>
+            <span className=" text-slate-700">TextPro</span>
           </h1>
         </Link>
         <form className=" bg-slate-100 p-3 rounded-lg flex items-center">
