c29d45ed24ac76b9ec52b92043bdb5795c60126b
guycohh <guycohh@gmail.com>
2024-04-26 17:50:02 +0300

    Fix UI, add filters for the displayed posts

--- client/src/components/UserPosts/UserPosts.jsx
+++ client/src/components/UserPosts/UserPosts.jsx
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
 const UserPosts = () => {
   const { currentUser, loading, error } = useSelector((state) => state.user);
   const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [visibilityFilter, setVisibilityFilter] = useState("all");
+  const [sortBy, setSortBy] = useState("newest");
 
   useEffect(() => {
     if (currentUser) {
@@ -48,7 +60,6 @@ const UserPosts = () => {
       });
       const data = await res.json();
       if (data.success) {
-        // Update the posts list to reflect the change
         setPosts(posts.map((post) => (post._id === postId ? data.post : post)));
       }
     } catch (error) {
@@ -56,23 +67,102 @@ const UserPosts = () => {
     }
   };
 
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h2 className="text-2xl font-bold mb-4">Your Posts</h2>
-      {loading && <p>Loading...</p>}
-      {error && <p className="text-red-500">Error: {error}</p>}
+  const applyFilter = () => {
+    // Filter posts based on the filter value and visibility filter
+    let filteredPosts = posts.filter((post) =>
+      post.title.toLowerCase().includes(filter.toLowerCase())
+    );
 
-      <div className="mt-8">
-        {posts.map((post) => (
+    if (visibilityFilter === "public") {
+      filteredPosts = filteredPosts.filter((post) => post.isPublic);
+    } else if (visibilityFilter === "private") {
+      filteredPosts = filteredPosts.filter((post) => !post.isPublic);
+    }
+
+    // Apply sorting based on filter
+    switch (sortBy) {
+      case "newest":
+        filteredPosts.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        break;
+      case "older":
+        filteredPosts.sort(
+          (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+        );
+        break;
+      case "mostLikes":
+        filteredPosts.sort((a, b) => b.likes.length - a.likes.length);
+        break;
+      default:
+        break;
+    }
+
+    return filteredPosts;
+  };
+
+  return (
+    <div className="grid grid-cols-6  md:grid-flow-row ">
+      <div className="col-span-6 md:col-span-6">
+        <h1 className="flex justify-center text-5xl">My Posts</h1>
+      </div>
+      {loading && <p className="col-span-6">Loading...</p>}
+      {error && <p className="col-span-6">Error: {error}</p>}
+      <div className="col-span-6 m-2 mr-1 md:col-span-1 bg-white rounded-lg shadow-lg shadow-cyan-500/50 dark:bg-gray-900 dark:text-gray-300 p-4">
+        <h1 className="mb-4 text-lg font-semibold">Filters</h1>
+        <input
+          type="text"
+          placeholder="Filter by title"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          className="w-full border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:ring focus:border-blue-300"
+        />
+        <select
+          value={visibilityFilter}
+          onChange={(e) => setVisibilityFilter(e.target.value)}
+          className="w-full border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          <option value="all">All</option>
+          <option value="public">Public</option>
+          <option value="private">Private</option>
+        </select>
+        <label className="block text-sm font-medium text-gray-700 mb-2">
+          Sort by:
+        </label>
+        <select
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+          className="w-full border border-gray-300 rounded-md px-4 py-2 mb-2 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          <option value="newest">Newest</option>
+          <option value="older">Older</option>
+          <option value="mostLikes">Most Likes</option>
+        </select>
+      </div>
+
+      <div className="col-span-6 md:col-span-5">
+        {applyFilter().map((post) => (
           <div
             key={post._id}
-            className="bg-white rounded-lg shadow-lg p-6 mb-4 flex"
+            className="flex flex-col gap-2 m-2 ml-1 p-4 bg-white rounded-lg shadow-lg shadow-cyan-500/50 dark:bg-gray-900 dark:text-gray-300"
           >
-            <div className="flex-grow">
-              <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
-              <p className="text-gray-700">{post.content}</p>
+            <div className="row-span-1">
+              <h3 className="text-lg font-semibold">{post.title}</h3>
+              <p className="text-sm text-gray-600">
+                {formatDate(post.createdAt)}
+              </p>
+              {/* Splitting content into paragraphs */}
+              {post.content.split("\n").map((paragraph, index) => (
+                <p key={index} className="text-base">
+                  {paragraph}
+                </p>
+              ))}
+              <p className="text-sm text-gray-600">
+                Likes: {post.likes.length}
+              </p>
             </div>
-            <div>
+
+            <div className="row-span-1 flex justify-center items-center">
               <button
                 onClick={() => toggleVisibility(post._id, post.isPublic)}
                 className={`${
