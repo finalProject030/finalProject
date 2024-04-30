01b775dfb9c18de4990c3b40e9081ea39dfcc081
guycohh <guycohh@gmail.com>
2024-04-30 15:12:43 +0300

    small issue

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,25 +1,87 @@
-const handleSubmit = async (e) => {
-  e.preventDefault();
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
-  try {
-    dispatch(signInStart());
-    const res = await fetch(
-      "https://finalproject-a66r.onrender.com/api/auth/signin",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      dispatch(signInStart());
+      const res = await fetch(
+        "https://finalproject-a66r.onrender.com/api/auth/signin",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
       }
-    );
-
-    const data = await res.json();
-    if (data.success === false) {
-      dispatch(signInFailure(data.message));
-      return;
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
-    dispatch(signInSuccess(data));
-    navigate("/");
-  } catch (error) {
-    dispatch(signInFailure(error.message));
-  }
-};
+  };
+
+  return (
+    <div className="bg-gradient-to-b from-gray-600 to-blue-300 min-h-screen flex flex-col justify-center items-center">
+      <div className="p-3 max-w-xl mx-auto w-full bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl text-center font-semibold my-7">SignIn</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="email"
+            placeholder="Email"
+            className="border p-3 rounded-lg"
+            id="email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border p-3 rounded-lg"
+            id="password"
+            onChange={handleChange}
+          />
+          <button
+            disabled={loading}
+            className="bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Loading..." : "Sign In"}
+          </button>
+          <OAuth />
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p>Don't have an account?</p>
+          <Link to={"/sign-up"}>
+            <span className="text-blue-700">Sign up</span>
+          </Link>
+        </div>
+        {error && <p className="text-red-500 mt-5">{error}</p>}
+      </div>
+    </div>
+  );
+}
