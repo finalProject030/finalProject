58b458b89265841cf8a77c0b60e0bf6c3a16132a
guycohh <guycohh@gmail.com>
2024-05-10 14:46:55 +0300

    buttons colors

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -198,7 +198,9 @@ const PostPage = () => {
                   toggleVisibility(post.post._id, post.post.isPublic)
                 }
                 className={`${
-                  post.post.isPublic ? "bg-green-500" : "bg-red-500"
+                  post.post.isPublic
+                    ? "text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                    : "text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                 } hover:bg-opacity-75 text-white font-semibold py-2 px-2 rounded mr-2`}
               >
                 {post.post.isPublic ? "Make Private" : "Make Public"}
@@ -206,7 +208,7 @@ const PostPage = () => {
 
               <button
                 onClick={handleDeleteButtonClick}
-                className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
               >
                 Delete
               </button>
@@ -218,14 +220,14 @@ const PostPage = () => {
               {editing ? (
                 <button
                   onClick={handleSaveButtonClick}
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                  className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
                 >
                   Save
                 </button>
               ) : (
                 <button
                   onClick={handleEditButtonClick}
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                  className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
                 >
                   Edit
                 </button>
