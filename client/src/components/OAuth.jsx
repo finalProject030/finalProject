d30accf11ab535a6e308c5114955305dda0120ba
guycohh <guycohh@gmail.com>
2024-05-16 21:20:23 +0300

    fix

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -14,14 +14,13 @@ import { urlServer } from "../variables";
 export default function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const googleAuthProvider = new GoogleAuthProvider();
 
   useEffect(() => {
     // Handle the redirect result when the component mounts
     const handleRedirectResult = async () => {
       try {
         const auth = getAuth(app);
-        const result = await getRedirectResult(auth, googleAuthProvider);
+        const result = await getRedirectResult(auth);
 
         console.log(auth);
 
