98069e704e97430c559ba63158e5f265683af5ab
finalproject030 <guycohh@gmail.com>
2024-05-18 21:25:38 +0300

    firebase change user

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXlJ-6y9UgkKU4XWsEA31B9fKeMGvC7IU",
-  authDomain: "mern-estate-3cab3.firebaseapp.com",
-  projectId: "mern-estate-3cab3",
-  storageBucket: "mern-estate-3cab3.appspot.com",
-  messagingSenderId: "343294743659",
-  appId: "1:343294743659:web:65cf5ff524a1741ddd4d2e",
+  apiKey: "AIzaSyAfEruRT_KzzuXXLtllFOyym3z29_tiOGo",
+  authDomain: "stacktextpro.firebaseapp.com",
+  projectId: "stacktextpro",
+  storageBucket: "stacktextpro.appspot.com",
+  messagingSenderId: "703905891648",
+  appId: "1:703905891648:web:aeff030a5fb43f003326a5",
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
