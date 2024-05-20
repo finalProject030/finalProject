36b74f0fa467608d62af05c38d1a1ff3850aa8bf
finalproject030 <guycohh@gmail.com>
2024-05-21 02:17:30 +0300

    update Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -120,8 +120,7 @@ const Footer = () => {
         </div>
         <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
-            © 2023 <a href="https://flowbite.com/">Flowbite™</a>. All Rights
-            Reserved.
+            © 2024 StackTextPro™. All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
             <a
