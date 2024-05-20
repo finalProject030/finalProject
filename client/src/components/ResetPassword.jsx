fdf7ce6ba0729f863fe0b3d6e73d6c560491f737
finalproject030 <guycohh@gmail.com>
2024-05-20 19:11:15 +0300

    validate passwords + UI

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -8,6 +8,22 @@ const ResetPassword = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState("");
 
+  const [isLengthValid, setIsLengthValid] = useState(false);
+  const [hasLowercase, setHasLowercase] = useState(false);
+  const [hasSpecialChar, setHasSpecialChar] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(false);
+
+  useEffect(() => {
+    console.log("Token from URL:", token);
+  }, [token]);
+
+  useEffect(() => {
+    setIsLengthValid(password.length >= 10);
+    setHasLowercase(/[a-z]/.test(password));
+    setHasSpecialChar(/[!@#?]/.test(password));
+    setPasswordsMatch(password === confirmPassword);
+  }, [password, confirmPassword]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("Submitting password reset");
@@ -60,9 +76,68 @@ const ResetPassword = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
+          <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
+            Password requirements:
+          </h2>
+          <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+            <li className="flex items-center">
+              <svg
+                className={`w-3.5 h-3.5 me-2 ${
+                  isLengthValid
+                    ? "text-green-500 dark:text-green-400"
+                    : "text-gray-500 dark:text-gray-400"
+                } flex-shrink-0`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+              </svg>
+              At least 10 characters
+            </li>
+            <li className="flex items-center">
+              <svg
+                className={`w-3.5 h-3.5 me-2 ${
+                  hasLowercase
+                    ? "text-green-500 dark:text-green-400"
+                    : "text-gray-500 dark:text-gray-400"
+                } flex-shrink-0`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+              </svg>
+              At least one lowercase character
+            </li>
+            <li className="flex items-center">
+              <svg
+                className={`w-3.5 h-3.5 me-2 ${
+                  hasSpecialChar
+                    ? "text-green-500 dark:text-green-400"
+                    : "text-gray-500 dark:text-gray-400"
+                } flex-shrink-0`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+              </svg>
+              At least one special character, e.g., ! @ # ?
+            </li>
+          </ul>
           <button
             type="submit"
             className="bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+            disabled={
+              !isLengthValid ||
+              !hasLowercase ||
+              !hasSpecialChar ||
+              !passwordsMatch
+            }
           >
             Reset Password
           </button>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
 import { urlServer } from "../variables";
 import Facebook from "../components/Facebook";
-import passwordValidator from "password-validator";
 
-export default function signUp() {
-  const [formData, setFormData] = useState({});
+export default function SignUp() {
+  const [formData, setFormData] = useState({
+    password: "",
+    confirmPassword: "",
+  });
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  const [isLengthValid, setIsLengthValid] = useState(false);
+  const [hasLowercase, setHasLowercase] = useState(false);
+  const [hasSpecialChar, setHasSpecialChar] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(false);
+
+  useEffect(() => {
+    setIsLengthValid(formData.password.length >= 10);
+    setHasLowercase(/[a-z]/.test(formData.password));
+    setHasSpecialChar(/[!@#?]/.test(formData.password));
+    setPasswordsMatch(formData.password === formData.confirmPassword);
+  }, [formData.password, formData.confirmPassword]);
 
   const handleChange = (e) => {
     setFormData({
@@ -20,25 +34,12 @@ export default function signUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const schema = new passwordValidator();
-    schema
-      .is()
-      .min(8)
-      .has()
-      .uppercase()
-      .has()
-      .lowercase()
-      .has()
-      .digits()
-      .oneOf(["Passw0rd", "Password123"]);
-
-    // if (!schema.validate(formData.password)) {
-    //   setError(
-    //     "Password is not strong enough. It should contain at least 8 characters, including uppercase and lowercase letters, numbers, and special symbols. Avoid using common passwords like 'Passw0rd' or 'Password123'."
-    //   );
-    //   return;
-    // }
+    if (!isLengthValid || !hasLowercase || !hasSpecialChar || !passwordsMatch) {
+      setError(
+        "Password does not meet requirements or passwords do not match."
+      );
+      return;
+    }
 
     try {
       setLoading(true);
@@ -64,46 +65,112 @@ export default function signUp() {
   };
 
   return (
-    <div className="p-3 max-w-lg mx-auto  ">
-      <h1 className="text-3xl text-center font-semibold my-7  ">SignUp</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
-          className="border p-3 rounded-lg "
+          className="border p-3 rounded-lg"
           id="username"
           onChange={handleChange}
-        ></input>
+        />
         <input
           type="email"
           placeholder="email"
-          className="border p-3 rounded-lg "
+          className="border p-3 rounded-lg"
           id="email"
           onChange={handleChange}
-        ></input>
+        />
         <input
           type="password"
           placeholder="password"
-          className="border p-3 rounded-lg "
+          className="border p-3 rounded-lg"
           id="password"
           onChange={handleChange}
-        ></input>
+        />
+        <input
+          type="password"
+          placeholder="confirm password"
+          className="border p-3 rounded-lg"
+          id="confirmPassword"
+          onChange={handleChange}
+        />
+        <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
+          Password requirements:
+        </h2>
+        <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+          <li className="flex items-center">
+            <svg
+              className={`w-3.5 h-3.5 me-2 ${
+                isLengthValid
+                  ? "text-green-500 dark:text-green-400"
+                  : "text-gray-500 dark:text-gray-400"
+              } flex-shrink-0`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            At least 10 characters
+          </li>
+          <li className="flex items-center">
+            <svg
+              className={`w-3.5 h-3.5 me-2 ${
+                hasLowercase
+                  ? "text-green-500 dark:text-green-400"
+                  : "text-gray-500 dark:text-gray-400"
+              } flex-shrink-0`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            At least one lowercase character
+          </li>
+          <li className="flex items-center">
+            <svg
+              className={`w-3.5 h-3.5 me-2 ${
+                hasSpecialChar
+                  ? "text-green-500 dark:text-green-400"
+                  : "text-gray-500 dark:text-gray-400"
+              } flex-shrink-0`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            At least one special character, e.g., ! @ # ?
+          </li>
+        </ul>
         <button
-          disabled={loading}
-          className=" bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 "
+          disabled={
+            loading ||
+            !isLengthValid ||
+            !hasLowercase ||
+            !hasSpecialChar ||
+            !passwordsMatch
+          }
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? "Loading..." : "Sign Up"}
         </button>
         <OAuth />
         <Facebook />
       </form>
-      <div className=" flex gap-2 mt-5">
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
-          <span className=" text-blue-700">Sign In</span>
+          <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
-      {error && <p className=" text-red-500 mt-5">{error}</p>}
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
