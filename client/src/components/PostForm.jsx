a2e8f8139c5aab9edd3bd757784bd9993a142dc4
finalproject030 <guycohh@gmail.com>
2024-05-23 14:13:53 +0300

    center the button

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -462,13 +462,15 @@ export default function PostCreationForm() {
                 </div>
                 {/* )}  */}
 
-                <button
-                  type="submit"
-                  className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                  disabled={loading || loading}
-                >
-                  Create My Post
-                </button>
+                <div className="flex justify-center w-full ">
+                  <button
+                    type="submit"
+                    className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                    disabled={loading || loading}
+                  >
+                    Create My Post
+                  </button>
+                </div>
               </form>
 
               <div className="flex justify-center w-full ">
