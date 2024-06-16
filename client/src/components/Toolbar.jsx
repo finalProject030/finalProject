5770ce37f12317123f9ff6f9f9630e3c19475531
finalproject030 <guycohh@gmail.com>
2024-06-16 21:08:16 +0300

    Fix toolbar UI

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -4,57 +4,77 @@ import { PiTargetBold } from "react-icons/pi";
 import { BsCheckCircleFill } from "react-icons/bs";
 
 const Toolbar = ({ currentStep }) => {
-  console.log(currentStep);
+  const getStepClass = (step) => {
+    if (currentStep === step) {
+      console.log("1");
+
+      return "bg-blue-100 text-blue-600 dark:bg-blue-800 dark:text-blue-300";
+    }
+    if (
+      step === "posts" ||
+      (step === "selectedPosts" &&
+        (currentStep === "selectedPosts" || currentStep === "postCreationForm"))
+    ) {
+      console.log("2");
+      return "bg-blue-100 text-blue-600 dark:bg-blue-800 dark:text-blue-300";
+    }
+    return "bg-gray-100 text-gray-500 dark:bg-gray-700 dark:text-gray-100";
+  };
+
+  const getLineClass = (step) => {
+    if (
+      (step === "posts" &&
+        (currentStep === "selectedPosts" ||
+          currentStep === "postCreationForm")) ||
+      (step === "selectedPosts" && currentStep === "postCreationForm")
+    ) {
+      return "bg-blue-100 dark:bg-blue-800";
+    }
+    return "bg-gray-100 dark:bg-gray-700";
+  };
 
   return (
     <div className="flex justify-center lg:mx-50 mx-4">
       <ol className="flex justify-center w-full">
-        <li
-          className={`relative flex w-full items-center ${
-            currentStep !== "posts"
-              ? "text-blue-600 dark:text-blue-500 after:w-full after:h-1 after:bg-blue-100 after:block dark:after:bg-blue-800"
-              : "after:w-full after:h-1 after:bg-gray-100 after:block dark:after:bg-gray-700"
-          }`}
-        >
-          <span className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
-            <FaMarker
-              className={`${
-                currentStep === "posts"
-                  ? "text-blue-600 lg:w-4 lg:h-4 dark:text-blue-300"
-                  : "text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100"
-              }`}
-            />
+        {/* Step 1: Posts */}
+        <li className="relative flex  items-center">
+          <span
+            className={`flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0 ${getStepClass(
+              "posts"
+            )}`}
+          >
+            <FaMarker className="lg:w-5 lg:h-5" />
           </span>
+          <div className={` left-1/2 h-1 w-full `} style={{ zIndex: -1 }} />
         </li>
 
-        <li
-          className={`relative flex w-full items-center ${
-            currentStep === "selectedPosts"
-              ? "text-blue-600 dark:text-blue-500 after:w-full after:h-1 after:bg-blue-100 after:block dark:after:bg-blue-800"
-              : "after:w-full after:h-1 after:bg-gray-100 after:block dark:after:bg-gray-700"
-          }`}
-        >
-          <span className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
-            <PiTargetBold
-              className={`${
-                currentStep === "postCreationForm"
-                  ? "text-blue-600 lg:w-4 lg:h-4 dark:text-blue-300"
-                  : "text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100"
-              }`}
-            />
+        {/* Step 2: Selected Posts */}
+        <li className="relative flex w-full items-center">
+          <div
+            className={` left-1/2 h-1 w-full ${getLineClass("selectedPosts")}`}
+            style={{ zIndex: -1 }}
+          />
+          <span
+            className={`flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0 ${getStepClass(
+              "selectedPosts"
+            )}`}
+          >
+            <PiTargetBold className="lg:w-5 lg:h-5" />
           </span>
+          <div
+            className={` left-1/2 h-1 w-full ${getLineClass("selectedPosts")}`}
+            style={{ zIndex: -1 }}
+          />
         </li>
 
+        {/* Step 3: Post Creation Form */}
         <li className="relative flex items-center">
-          <span className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
-            <BsCheckCircleFill
-              className={`${
-                currentStep === "selectedPosts" ||
-                currentStep === "postCreationForm"
-                  ? "text-blue-600 lg:w-4 lg:h-4 dark:text-blue-300"
-                  : "text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100"
-              }`}
-            />
+          <span
+            className={`flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0 ${getStepClass(
+              "postCreationForm"
+            )}`}
+          >
+            <BsCheckCircleFill className="lg:w-5 lg:h-5" />
           </span>
         </li>
       </ol>
