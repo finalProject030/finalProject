e8a4cef861ea4ac01e9b0887c49da0f5f3391b9f
finalproject030 <guycohh@gmail.com>
2024-07-25 20:36:26 +0300

    fix feed UI

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -194,81 +194,90 @@ const Feed = () => {
   return (
     <>
       <div className="container my-20 mx-auto bg-gray-100 rounded-lg border border-gray-200 px-4 py-8 min-h-screen">
-        {/* Header, loader, error message, etc. */}
-        <div className="mt-8 grid gap-8">
-          {publicPosts.map((post) => (
-            <div
-              key={post._id}
-              className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-            >
-              <div className="flex justify-between items-center mb-5 text-gray-500">
-                <span className="text-sm">
-                  {format(new Date(post.createdAt), "MMMM d, yyyy")} (
-                  {formatDistanceToNow(new Date(post.createdAt))} ago)
-                </span>
-              </div>
-              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                <a>{post.title}</a>
-              </h2>
-              {post.image && (
-                <img
-                  src={post.image}
-                  alt={post.title}
-                  className="w-full h-48 object-cover rounded-lg mb-4 cursor-pointer"
-                  onClick={() => openModal(post.image)}
-                />
-              )}
-              <div className="mb-5 font-light text-gray-500 dark:text-gray-400">
-                {post.content.split("\n").map((paragraph, index) => (
-                  <p key={index}>{paragraph}</p>
-                ))}
-              </div>
+        {loading ? (
+          <div className="flex items-center justify-center min-h-screen">
+            <PuffLoader color="#3498db" size={60} />
+          </div>
+        ) : (
+          <div className="mt-8 grid gap-8">
+            <h1 className="text-7xl text-center mb-2  font-bold border-4 border-gray rounded-lg p-4 shadow-lg bg-gradient-to-r from-gray-300 to-white text-black">
+              Feed
+            </h1>
 
-              <div className="flex flex-col lg:flex-row items-center lg:justify-between mb-3">
-                {post.profilePictureURL && (
-                  <div className="flex items-center mb-2 lg:mb-0">
-                    <img
-                      className="w-7 h-7 rounded-full mr-2"
-                      src={post.profilePictureURL}
-                      alt="Profile"
-                    />
-                    <span className="font-medium dark:text-white">
-                      {post.userName}
-                    </span>
-                  </div>
+            {publicPosts.map((post) => (
+              <div
+                key={post._id}
+                className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+              >
+                <div className="flex justify-between items-center mb-5 text-gray-500">
+                  <span className="text-sm">
+                    {format(new Date(post.createdAt), "MMMM d, yyyy")} (
+                    {formatDistanceToNow(new Date(post.createdAt))} ago)
+                  </span>
+                </div>
+                <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a>{post.title}</a>
+                </h2>
+                {post.image && (
+                  <img
+                    src={post.image}
+                    alt={post.title}
+                    className="w-full h-48 object-cover rounded-lg mb-4 cursor-pointer"
+                    onClick={() => openModal(post.image)}
+                  />
                 )}
+                <div className="mb-5 font-light text-gray-500 dark:text-gray-400">
+                  {post.content.split("\n").map((paragraph, index) => (
+                    <p key={index}>{paragraph}</p>
+                  ))}
+                </div>
 
-                <div className="flex items-center gap-3">
-                  <p className="text-gray-400 mr-4 lg:mb-0 lg:mr-0">
-                    {post.likes.length} Likes
-                  </p>
-                  {post.loading ? (
-                    <button
-                      className="flex ml-3 items-center bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded transition duration-300 transform hover:scale-105 mb-2 lg:mb-0"
-                      disabled
-                    >
-                      Loading...
-                    </button>
-                  ) : post.userLiked ? (
-                    <button
-                      onClick={() => handleDislikePost(post._id)}
-                      className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                    >
-                      <BiDislike className="" />
-                    </button>
-                  ) : (
-                    <button
-                      onClick={() => handleLikePost(post._id)}
-                      className="text-white bg-gradient-to-r mt-4 from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                    >
-                      <BiLike className="" />
-                    </button>
+                <div className="flex flex-col lg:flex-row items-center lg:justify-between mb-3">
+                  {post.profilePictureURL && (
+                    <div className="flex items-center mb-2 lg:mb-0">
+                      <img
+                        className="w-7 h-7 rounded-full mr-2"
+                        src={post.profilePictureURL}
+                        alt="Profile"
+                      />
+                      <span className="font-medium dark:text-white">
+                        {post.userName}
+                      </span>
+                    </div>
                   )}
+
+                  <div className="flex items-center gap-3">
+                    <p className="text-gray-400 mr-4 lg:mb-0 lg:mr-0">
+                      {post.likes.length} Likes
+                    </p>
+                    {post.loading ? (
+                      <button
+                        className="flex ml-3 items-center bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded transition duration-300 transform hover:scale-105 mb-2 lg:mb-0"
+                        disabled
+                      >
+                        Loading...
+                      </button>
+                    ) : post.userLiked ? (
+                      <button
+                        onClick={() => handleDislikePost(post._id)}
+                        className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                      >
+                        <BiDislike className="" />
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => handleLikePost(post._id)}
+                        className="text-white bg-gradient-to-r mt-4 from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                      >
+                        <BiLike className="" />
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
 
         {modalOpen && <Modal imageSrc={modalImage} onClose={closeModal} />}
       </div>
