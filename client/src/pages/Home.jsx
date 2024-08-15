fcc8bace63325e394584ca5b31575e0030d375cb
finalproject030 <guycohh@gmail.com>
2024-08-15 13:27:47 +0300

    add video Modal

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,10 +8,13 @@ import video1 from "../../assets/Dashboard.png";
 import MybMA from "../../assets/MybMA.png";
 import gemini from "../../assets/gemini.webp";
 import ScrollToTop from "../components/ScrollToTop";
+import ModalVideo from "react-modal-video";
+import "react-modal-video/css/modal-video.min.css";
 
 const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
   const imgRef = useRef(null);
+  const [isOpen, setOpen] = useState(false);
 
   const handleScroll = () => {
     if (imgRef.current) {
@@ -33,8 +36,6 @@ const Home = () => {
     <div className="relative">
       {/* Main Content */}
       <div className="relative bg-gradient-to-b from-gray-600 to-blue-300 flex flex-col justify-center items-center text-white">
-        {/* Video Background */}
-
         {/* Overlay */}
         <div className="absolute w-full h-full skew-y-12 origin-right z-10 bg-white dark:bg-gray-900 inset-0">
           {/* First Span */}
@@ -78,25 +79,38 @@ const Home = () => {
         </div>
       </div>
 
-      {/* Clickable Image with Animation */}
+      {/* Clickable Image with Modal Video */}
       <div className="flex flex-col justify-center items-center bg-gray-50 dark:bg-gray-900 p-4">
-        <h2 className="text-4xl text-black font-extrabold mb-6 mt-6 dark:text-white">
-          Click On Me!
-        </h2>
-        <a
-          href="https://drive.google.com/file/d/1-rInqCt2Rh2RvF9SPOpgnRmBFhUUTdpB/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="relative cursor-pointer" onClick={() => setOpen(true)}>
           <img
             ref={imgRef}
             src={video1}
             alt="Video Thumbnail"
-            className={`w-54 h-auto cursor-pointer transition-opacity duration-1000 ${
+            className={`w-54 h-auto transition-opacity duration-1000 ${
               isVisible ? "opacity-100 animate-fadeIn" : "opacity-0"
             }`}
           />
-        </a>
+          <h2 className="absolute inset-0 text-black flex items-center justify-center text-4xl font-extrabold opacity-90">
+            <svg
+              viewBox="10 146.5 980.1 707"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-40 h-40 transform transition-transform duration-300 hover:scale-110 hover:text-red-500" // Added hover effect
+              fill="currentColor"
+            >
+              <path d="m818.9 146.5h-637.8c-94.4 0-171.1 76.8-171.1 171.1v364.8c0 94.3 76.7 171.1 171.1 171.1h637.9c94.3 0 171.1-76.8 171.1-171.1v-364.8c-.1-94.3-76.8-171.1-171.2-171.1zm101 535.9c0 55.7-45.3 101-100.9 101h-637.9c-55.7 0-101-45.3-101-101v-364.8c0-55.7 45.3-101 101-101h637.9c55.7 0 100.9 45.3 100.9 101z" />
+              <path d="m688.2 470.7-259.9-171.3c-10.8-7.1-24.6-7.7-35.9-1.6-11.4 6.1-18.4 18-18.4 30.9v342.6c0 12.9 7.1 24.7 18.4 30.9 5.2 2.8 10.9 4.2 16.6 4.2 6.7 0 13.5-1.9 19.3-5.8l259.9-171.3c9.8-6.5 15.8-17.5 15.8-29.3-.1-11.8-6-22.8-15.8-29.3zm-244.1 135.5v-212.4l161.1 106.2z" />
+            </svg>
+          </h2>
+        </div>
+
+        {/* Modal Video */}
+        <ModalVideo
+          channel="youtube"
+          youtube={{ mute: 0, autoplay: 0 }}
+          isOpen={isOpen}
+          videoId="RVVnzM53_CA"
+          onClose={() => setOpen(false)}
+        />
       </div>
 
       {/* Diagonal Background */}
@@ -106,8 +120,16 @@ const Home = () => {
             isVisible ? "opacity-100 animate-fadeIn" : "opacity-0"
           }`}
         >
-          <img src={MybMA} alt="?" className=" h-1/4 object-cover z-10 m-2" />
-          <img src={gemini} alt="?" className=" h-1/4 object-cover z-10 m-2" />
+          <img
+            src={MybMA}
+            alt="MybMA"
+            className=" h-1/4 object-cover z-10 m-2"
+          />
+          <img
+            src={gemini}
+            alt="Gemini"
+            className=" h-1/4 object-cover z-10 m-2"
+          />
         </div>
 
         <div className="absolute top-0 right-0 w-full h-full bg-gray-50 dark:bg-gray-900 transform -skew-y-12 z-0"></div>
@@ -118,7 +140,7 @@ const Home = () => {
         <p></p>
       </div>
 
-      {/* Contact Form (if needed) */}
+      {/* Contact Form */}
       <ContactForm />
       <ScrollToTop />
     </div>
