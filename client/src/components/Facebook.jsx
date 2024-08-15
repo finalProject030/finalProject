a499d9005c62d1cb950c74a7c610d28c5ddf892a
Shahar <shaharamr1041@gmail.com>
2024-08-15 16:03:40 +0300

    update

--- client/src/components/Facebook.jsx
+++ client/src/components/Facebook.jsx
@@ -5,10 +5,8 @@ import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
 import { scrollToTop, urlServer } from "../variables";
-import FacebookIcon from '@mui/icons-material/Facebook';
+import FacebookIcon from "@mui/icons-material/Facebook";
 import LoadingSpinner from "./LoadingSpinner"; // Import the LoadingSpinner component
-
-
 
 const Facebook = () => {
   const dispatch = useDispatch();
@@ -56,27 +54,28 @@ const Facebook = () => {
   };
 
   return (
-<div>
-
-{loading ? (
-  <LoadingSpinner /> // Render the loading spinner when loading
-) : (
-<a
-onClick={handleFacebookClick}
-role="button" // Add role attribute for accessibility
-data-icon="facebook"
->
-  <svg viewBox="0 0 24 24" 
-  className="w-9 h-9 rounded-full hover:bg-gray-400" >
-    <path fill-rule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm2.52-14.923v1.686h-1.004c-.366 0-.613.077-.74.23-.128.153-.192.383-.192.69v1.207h1.871l-.249 1.891h-1.622v4.849h-1.955V12.78H9v-1.89h1.629V9.497c0-.792.221-1.407.664-1.843.443-.437 1.033-.655 1.77-.655.626 0 1.111.026 1.456.077z">
-      </path>
-      </svg>
-      </a>
-
-  )}
-  </div>
-  )
-
+    <div>
+      {loading ? (
+        <LoadingSpinner /> // Render the loading spinner when loading
+      ) : (
+        <a
+          onClick={handleFacebookClick}
+          role="button" // Add role attribute for accessibility
+          data-icon="facebook"
+        >
+          <svg
+            viewBox="0 0 24 24"
+            className="w-9 h-9 rounded-full hover:bg-gray-400"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm2.52-14.923v1.686h-1.004c-.366 0-.613.077-.74.23-.128.153-.192.383-.192.69v1.207h1.871l-.249 1.891h-1.622v4.849h-1.955V12.78H9v-1.89h1.629V9.497c0-.792.221-1.407.664-1.843.443-.437 1.033-.655 1.77-.655.626 0 1.111.026 1.456.077z"
+            ></path>
+          </svg>
+        </a>
+      )}
+    </div>
+  );
 };
 
 export default Facebook;
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -6,8 +6,7 @@ import { signInSuccess } from "../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
 import LoadingSpinner from "./LoadingSpinner"; // Import the LoadingSpinner component
 import { scrollToTop, urlServer } from "../variables";
-import GoogleIcon from '@mui/icons-material/Google';
-
+import GoogleIcon from "@mui/icons-material/Google";
 
 export default function OAuth() {
   const dispatch = useDispatch();
@@ -40,7 +39,6 @@ export default function OAuth() {
       localStorage.setItem("token", data.token);
       navigate("/");
       scrollToTop();
-
     } catch (error) {
       setLoading(false);
       console.log("could not sign in with google", error);
@@ -51,30 +49,26 @@ export default function OAuth() {
 
   return (
     <div>
-
       {loading ? (
         <LoadingSpinner /> // Render the loading spinner when loading
       ) : (
-
         <a
-        onClick={handleGoogleClick}
-        data-icon="google-c"
-        role="button" // Add role attribute for accessibility
-        aria-disabled={loading} // Set aria-disabled attribute for accessibility
-      >
-        <svg viewBox="0 0 24 24" className="w-9 h-9 rounded-full hover:bg-gray-400" >
-        
-          <path fillRule="evenodd" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm.044-5.213c2.445 0 4.267-1.551 4.556-3.781v-1.891h-4.519v1.89h2.602a2.893 2.893 0 0 1-2.724 1.93c-1.194 0-2.677-1.1-2.677-2.843 0-1.621 1.161-2.876 2.677-2.876.739 0 1.413.279 1.922.736l1.399-1.376a4.744 4.744 0 1 0-3.236 8.212z" />
-        </svg>
-      </a>
-      
-
-
+          onClick={handleGoogleClick}
+          data-icon="google-c"
+          role="button" // Add role attribute for accessibility
+          aria-disabled={loading} // Set aria-disabled attribute for accessibility
+        >
+          <svg
+            viewBox="0 0 24 24"
+            className="w-9 h-9 rounded-full hover:bg-gray-400"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm.044-5.213c2.445 0 4.267-1.551 4.556-3.781v-1.891h-4.519v1.89h2.602a2.893 2.893 0 0 1-2.724 1.93c-1.194 0-2.677-1.1-2.677-2.843 0-1.621 1.161-2.876 2.677-2.876.739 0 1.413.279 1.922.736l1.399-1.376a4.744 4.744 0 1 0-3.236 8.212z"
+            />
+          </svg>
+        </a>
       )}
-
-
-
     </div>
-
   );
 }
