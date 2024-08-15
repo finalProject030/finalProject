e5a5de18d7d43bd2f5c50572bf61451f6bb34882
Shahar <shaharamr1041@gmail.com>
2024-08-15 15:59:14 +0300

    update

--- client/src/components/Facebook.jsx
+++ client/src/components/Facebook.jsx
@@ -56,56 +56,6 @@ const Facebook = () => {
   };
 
   return (
-    // <button
-    //   onClick={handleFacebookClick}
-    //   type="button"
-    //   disabled={loading}
-    //   className={`text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2 ${
-    //     loading ? "opacity-50 cursor-not-allowed" : ""
-    //   }`}
-    // >
-    //   <FacebookIcon />
-    //   {loading ? (
-    //     <svg
-    //       className="animate-spin h-5 w-5 mr-3"
-    //       xmlns="http://www.w3.org/2000/svg"
-    //       fill="none"
-    //       viewBox="0 0 24 24"
-    //     >
-    //       <circle
-    //         className="opacity-25"
-    //         cx="12"
-    //         cy="12"
-    //         r="10"
-    //         stroke="currentColor"
-    //         strokeWidth="4"
-    //       ></circle>
-    //       <path
-    //         className="opacity-75"
-    //         fill="currentColor"
-    //         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM12 20v4c4.418 0 8-3.582 8-8h-4c0 2.21-.898 4.21-2.351 5.657L12 20zm0-20l-3 2.647A7.962 7.962 0 014 12h4c0-3.042 1.135-5.824 3-7.938L12 0z"
-    //       ></path>
-    //     </svg>
-    //   ) : (
-    //     <svg
-    //       className="w-4 h-4 me-2"
-    //       aria-hidden="true"
-    //       xmlns="http://www.w3.org/2000/svg"
-    //       fill="currentColor"
-    //       viewBox="0 0 8 19"
-    //     >
-    //       <path
-    //         fillRule="evenodd"
-    //         d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-    //         clipRule="evenodd"
-    //       />
-    //     </svg>
-    //   )}
-    //   {loading ? "Signing In..." : "Continue with Facebook"}
-    // </button>
-
-
-
 <div>
 
 {loading ? (
