00fefb082a3c696b8ff90b1fed7b3881ec0c9f0e
Shahar <shaharamr1041@gmail.com>
2024-08-18 02:44:51 +0300

    sd

--- client/src/components/Facebook.jsx
+++ client/src/components/Facebook.jsx
@@ -58,14 +58,12 @@ const Facebook = () => {
       {loading ? (
         <LoadingSpinner /> // Render the loading spinner when loading
       ) : (
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            handleFacebookClick();
-          }}
-          // role="button" // Add role attribute for accessibility
+        <a
+          onClick={handleFacebookClick}
+          role="button" // Add role attribute for accessibility
           data-icon="facebook"
           aria-disabled={loading} // Set aria-disabled attribute for accessibility
+          tabIndex="0" // Ensure the element can be focused
         >
           <svg
             viewBox="0 0 24 24"
@@ -76,7 +74,7 @@ const Facebook = () => {
               d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm2.52-14.923v1.686h-1.004c-.366 0-.613.077-.74.23-.128.153-.192.383-.192.69v1.207h1.871l-.249 1.891h-1.622v4.849h-1.955V12.78H9v-1.89h1.629V9.497c0-.792.221-1.407.664-1.843.443-.437 1.033-.655 1.77-.655.626 0 1.111.026 1.456.077z"
             ></path>
           </svg>
-        </button>
+        </a>
       )}
     </div>
   );
